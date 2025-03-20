2c6df7733f72710bd857ddc7bd48c8ce21088a75
Leo <leo.battalora@gmail.com>
2025-03-19 22:45:01 -0400

    initialize repo with a poetry service and nextjs ui

---
+++ not-chipotle-service/src/not_chipotle_service/__main__.py
@@ -0,0 +1,8 @@
+def main():
+    """Entry point for the NotChipotle service."""
+    print("Starting my awesome service using __main__.py!")
+    # Your service initialization and logic here
+
+
+if __name__ == "__main__":
+    main()
